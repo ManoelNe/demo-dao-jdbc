92538b1b58013f15d7a9aa203afb43782936f75b
ManoelNe <manoel_neto@outlook.com>
2025-10-28 15:54:27 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,25 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		Seller sel = new Seller(
+			    2, 
+			    "Manoel", 
+			    "manoel_neto@outlook.com", 
+			    new Date(),       // birthDate
+			    50000.0,          // baseSalary
+			    obj               // Department
+			);
 		System.out.println(obj);
+		System.out.println(sel);
 
 	}
 
