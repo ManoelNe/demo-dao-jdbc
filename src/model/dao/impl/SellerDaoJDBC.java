51e6671f2b08ed4b8603b7fca52642b409eb4d2e
ManoelNe <manoel_neto@outlook.com>
2025-11-03 10:44:41 -0300

    SellerDaoJDBC and DaoFactor

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entites.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void inseert(Seller sel) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller sel) {
+		
+		
+	}
+
+	@Override
+	public void deletById(Integer id) {
+	
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+
+}
