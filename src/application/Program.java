d4dbfdeb0339e1a0b36e133bc194136a3c5342cf
ManoelNe <manoel_neto@outlook.com>
2025-11-05 13:53:53 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,21 @@
 package application;
 
-import java.util.Date;
 
-import model.entites.Department;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller sel = new Seller(
-			    2, 
-			    "Manoel", 
-			    "manoel_neto@outlook.com", 
-			    new Date(),       // birthDate
-			    50000.0,          // baseSalary
-			    obj               // Department
-			);
-		System.out.println(obj);
-		System.out.println(sel);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(5);
+		
+		System.out.println(seller);
 
 	}
 
---
+++ src/db/DB.java
@@ -0,0 +1,65 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DB {
+
+    private static Connection conn = null; 
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+
+               
+                if (!url.contains("?")) {
+                    url += "?useSSL=false&allowPublicKeyRetrieval=true";
+                } else {
+                    url += "&useSSL=false&allowPublicKeyRetrieval=true";
+                }
+
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+	public static void closeStatement(PreparedStatement st) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void closeResultSet(ResultSet rs) {
+		// TODO Auto-generated method stub
+		
+	}
+}
